a5862e72734f075e4151cb554b9d631b1df260e8
Doktor Jakub Szajder <kuba.w108@gmail.com>
2017-11-01 18:03:17 +0100

    update

--- cr_cp.sh
+++ cr_cp.sh
@@ -1,4 +1,4 @@
 #create copy of current vim configuration
-sudo cp -r "/home/jakub/.vim" "./vim"
-sudo cp  "/home/jakub/.vimrc" "./vimrc"
+cp -r "/home/jakub/.vim" "./vim"
+cp  "/home/jakub/.vimrc" "./vimrc"
 
